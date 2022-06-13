82af2c6e6a2d39fbfbdd2f4e7e42eb131ff9f34e
Mahesh Reddy Avula <107297986+MaheshReddyAvula@users.noreply.github.com>
2022-06-13 17:35:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,5 @@ streamlit.dataframe(my_data_rows)
 Snow_fruit_choice = streamlit.text_input('What fruit would you like to add?','kiwi')
 streamlit.write('Thanks for adding ', Snow_fruit_choice)
 
+my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+
